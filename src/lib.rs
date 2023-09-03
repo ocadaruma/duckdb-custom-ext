f4e410900b654b0c142494557b48bd94c713462c
Haruki Okada <ocadaruma@gmail.com>
2023-09-03 11:09:07 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 #[no_mangle]
-pub extern "C" fn libduckdb_custom_ext(_db: *mut std::ffi::c_void) {
+pub extern "C" fn libduckdb_custom_ext_init(_db: *mut std::ffi::c_void) {
     println!("hello from extension!");
 }
